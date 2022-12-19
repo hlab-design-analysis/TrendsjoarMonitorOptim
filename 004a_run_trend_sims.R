d60b03b77202ab7d0d5ea91beca68a38372f4439
nmprista <31890653+nmprista@users.noreply.github.com>
2022-12-19 11:30:35 +0100

    Update 004a_run_trend_sims.R

--- 004a_run_trend_sims.R
+++ 004a_run_trend_sims.R
@@ -9,50 +9,48 @@ rm(list=ls()); gc()
 # loads packages
 library(data.table)
 library(Kendall)
+  library(parallel) # detectCores
   library(foreach) # parallel computing
   library(doMC) # parallel computing
   library(doRNG) # Generic Reproducible Parallel Backend for 'foreach' Loops
 
+# auto: loads functions
 source("000_Funs/func_do_job_trend.r")
-
-# settings
-parallel::detectCores(all.tests = FALSE, logical = TRUE)
-parallel_type <- "foreach_mc" # "foreach_mc" # "none"
 
 # user: select site 
 site <- "Stensjön"	
 
-keep_objects <- c("keep_objects", "nsim", "cpus", "target_omdrev","sim_pop",
-                 "site","ptc0", "dt0","dir_outputs","do_job_trend")	
-
- 
 # user: sets dirs
 	dir_inputs_sim_res<-"003_SamplingSims/"
 	dir_outputs<-"004_TrendSims/"; 
-	#if(!file.exists(dir_outputs)) dir.create(dir_outputs, showWarnings = FALSE, recursive=T)
 
-# user: define omdrev
-target_omdrev<-c(1)
-if(any(!target_omdrev %in% c(1:2))) print("omdrev not define")
+# user: define sampling frequency (1 for annual, 2 for every other year)
+target_sampfreq<-c(1,2)
+if(any(!target_sampfreq %in% c(1:2))) print("omdrev not define")
 
+# user: select nsims (used to load sim results - should be the same as set in 003a)
+	nsims<-5 # 10000
  
-  # loads simulation data
-  sim_pop<-readRDS(file=paste(dir_inputs_sim_res, site, "_3sims_res_pop.rds", sep="")) 
+  # auto: loads simulation data
+  sim_pop<-readRDS(file=paste(dir_inputs_sim_res, site, "_",nsims,"sims_res_pop.rds", sep="")) 
 
-# loads simulations
-	load(file=paste(dir_inputs_sim_res, site, "_3sims_res_pop_prep.RData", sep="")) 
+# auto: loads simulations
+	load(file=paste0(dir_inputs_sim_res, site,"_",nsims,"sims_res_pop_prep.RData", sep="")) 
  
- 
+# user: settings
 	# user: number of simulations to run
-	nsim <- 3 # 1000
+	nsims_trend <- 5 # 10000
 	# cpus
 		# info only: number of cpus available
-		n_cpus<-detectCores(all.tests = FALSE, logical = TRUE); print(n_cpus)
+		totcpus<-detectCores(all.tests = FALSE, logical = TRUE); print(totcpus)
 		# user: number of cpus that should be used [defaults to half number available but can be changed by user]
-		cpus <- floor(n_cpus/2) # can be set independently by user
-		cpus <- 1 # can be set independently by user
+		ncpus <- floor(totcpus/2) # can be set independently by user
+		#ncpus <- 1 # can be set independently by user
  
-  
+ # auto: defines a few objects to keep during memory clean ups
+ keep_objects <- c("keep_objects", "nsims", "nsims_trend", "ncpus", "target_sampfreq","sim_pop",
+                 "site","ptc0", "dt0","dir_outputs","do_job_trend")	
+ 
   # prepares dataset
   dt0 <- sim_pop[, `:=`(ID = paste(SampSize, variable)),]
   
@@ -60,10 +58,10 @@ if(any(!target_omdrev %in% c(1:2))) print("omdrev not define")
   rm(sim_pop); gc()
   
 
-  for(omdrev in target_omdrev)
+  for(sampfreq in target_sampfreq)
   {  
     
-    cat(paste("#===============================\n", "omdrev ", omdrev,"\n#===============================\n"))  
+    cat(paste("#===============================\n", "sampfreq ", sampfreq,"\n#===============================\n"))  
     
     ls1<-split(dt0, dt0$ID) 
     
@@ -71,15 +69,11 @@ if(any(!target_omdrev %in% c(1:2))) print("omdrev not define")
       ptc<-Sys.time()	
       
       # initiates parallel
-      registerDoMC(cpus)
+      registerDoMC(ncpus)
       
-      # starts the seeds
-      #set.seed(123)			
-      # job
-      
-      res_model <- foreach (i=1:nsim, .options.RNG=123) %dorng% {
-        #require(data.table)
-        do_job_trend(lo=i, ls1 = ls1, omdrev)
+     
+      res_model <- foreach (i=1:nsims_trend, .options.RNG=123) %dorng% {
+        do_job_trend(lo=i, ls1 = ls1, sampfreq)
       }
       gc()
       res_model<-rbindlist(res_model)
@@ -89,7 +83,7 @@ if(any(!target_omdrev %in% c(1:2))) print("omdrev not define")
       
     cat("saving RDS...\n")
     ptc<-Sys.time()  	
-    saveRDS(res_model, file = paste(dir_outputs,site,"_omdrev_",omdrev,"_", nsim,"_regrsims_res_model.rds",sep=""))		
+    saveRDS(res_model, file = paste(dir_outputs,site,"_sampfreq",sampfreq,"_", nsims,"_", nsims_trend,"_trendsims_res_model.rds",sep=""))		
     print(Sys.time()-ptc)			
     
     #Sys.sleep(180)
