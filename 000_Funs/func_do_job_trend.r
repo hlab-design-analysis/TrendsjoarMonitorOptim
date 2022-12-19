498683178938d3d5aee201ec0143fcc859f5eee0
nmprista <31890653+nmprista@users.noreply.github.com>
2022-12-19 11:29:54 +0100

    Update func_do_job_trend.r

--- 000_Funs/func_do_job_trend.r
+++ 000_Funs/func_do_job_trend.r
@@ -1,13 +1,13 @@
     
 	do_job_trend<-function(lo, ls1, years){
 
-      out<-rbindlist(lapply(ls1, function(x, omdrev1 = omdrev) {
+      out<-rbindlist(lapply(ls1, function(x, sampfreq1 = sampfreq) {
         # selects years
-        # samples from 1:omdrev1 to select the starting point of the series
-        # samples series with the same length [eg, if years 2002:2018 (i.e., 17 years) and omdrev = 2 samples 8 years; if omdrev = 3 samples 5 years
-         if(omdrev1 %in% c(2,3)) 
+        # samples from 1:sampfreq1 to select the starting point of the series
+        # samples series with the same length [eg, if years 2002:2018 (i.e., 17 years) and sampfreq = 2 samples 8 years;
+         if(sampfreq1 %in% c(2,3)) 
            {
-           years_index<-seq(sample(1:omdrev1,1), length(unique(x$Year))%/%omdrev1*omdrev1, by = omdrev1) 
+           years_index<-seq(sample(1:sampfreq1,1), length(unique(x$Year))%/%sampfreq1*sampfreq1, by = sampfreq1) 
            } else {
                   years_index<-as.numeric(as.factor(unique(x$Year)))
                   }
@@ -25,7 +25,7 @@
         
         
         # stores results 
-        data.table(SampSize = x$SampSize[1], variable = x$variable[1], omdrev = omdrev1, years = paste(dt2$Year, collapse=","), 
+        data.table(SampSize = x$SampSize[1], variable = x$variable[1], sampfreq = sampfreq1, years = paste(dt2$Year, collapse=","), 
                                     linear_slope = coef(model.linear)[2], linear_pvalue = summary(model.linear)[[4]][8], linear_sign = summary(model.linear)[[4]][8]<0.05, 
                                         #kendall_tau1 =  model.kendall1[[4]][[1]], kendall_sign1 = model.kendall1[[3]][[1]]<0.05,
                                           kendall_tau =  model.kendall[[1]][[1]], kendall_pvalue = model.kendall[[2]][[1]], kendall_sign = model.kendall[[2]][[1]]<0.05)
