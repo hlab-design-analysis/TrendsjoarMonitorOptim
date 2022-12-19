bfb77fb28dac3c1a02b0237c592059c3c840dd1d
nmprista <31890653+nmprista@users.noreply.github.com>
2022-12-19 09:18:55 +0100

    Update 000_create_main_directories.R

--- 000_create_main_directories.R
+++ 000_create_main_directories.R
@@ -1,5 +1,5 @@
 # ==============================
-# Sets up directory structure
+# Sets up directory structure and checks availability of main functions
 # Nuno Prista, SLU Aqua, 2022 @ Konsekvensanalysis project
 	# based on developments under Nuno Prista, SLU Aqua, 2019, 2022 @ KustMonitoring project
 # ==============================
@@ -7,14 +7,15 @@
 # checks if directory for 000_Funs exists [if not, creates it]
 if(!file.exists("000_Funs")) dir.create("000_Funs")
 
-# checks of 000_Funs has the necessary functions (should yield TRUE) [note: these need to manually copied]
+# checks if 000_Funs has the necessary functions (should yield TRUE) [note: these need to manually copied]
 required_funs<-c("func_do_summary_stratified_mean_time_series.r",
 			"func_do_additional_calcs_slab.r",
-			"func_do_job.r",	
+			"func_do_job_sampling.r",
+			"func_do_job_trend.r",	
 			"func_do_sampling_stratified.r",
+			"func_do_summary_stratified_mean.r",
 			"func_do_sampling_stratified_timeseries.r",
-			"func_sampleFromAGroup.r",
-			"func_do_summary_stratified_mean.r"
+			"func_sampleFromAGroup.r"
 			)
 all(file.exists(paste("000_Funs",required_funs,sep="/")))
 
